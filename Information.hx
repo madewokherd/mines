d11eeb52004ef2dfcee51048c418570128002fb6
Vincent Povirk <madewokherd@gmail.com>
2016-02-01 14:20:30 -0600

    Information: Add hashCode() and equals().

--- Information.hx
+++ Information.hx
@@ -8,4 +8,12 @@ class Information {
         this.spaces = spaces;
         this.count = count;
     }
+
+    public function hashCode(): Int {
+        return spaces.hashCode() + count;
+    }
+
+    public function equals(other: Information) : Bool {
+        return count == other.count && (spaces == other.spaces || spaces.equals(other.spaces));
+    }
 }
